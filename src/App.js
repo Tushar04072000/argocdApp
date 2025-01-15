aac1119bbcb5df357099233698df871c0c8e9c76
Tushar04072000 <130157133+Tushar04072000@users.noreply.github.com>
2025-01-15 15:31:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Tushar Patil
+          Hello Tushar
         </p>
         <a
           className="App-link"
